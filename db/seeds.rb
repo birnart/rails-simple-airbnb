1f5ad4e2d18c43a6419cbb86bb9dd27ffe447ade
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2020-11-11 16:48:09 +0100

    saving this

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. khbaskbdkb beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'my cool flat',
+  address: 'volkhartstrsse 2b münchen',
+  description: 'lit lil garden and two people appartment with kitchen and stuff and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'crease',
+  address: '10 Clifton Gardens munich W9 1DT',
+  description: 'Alit shit, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 20,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'cool place hre',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. khbaskbdkb beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'thjis ais party town',
+  address: 'volkhartstrsse 2b münchen',
+  description: 'lit lil garden and two people appartment with kitchen and stuff and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'yeaskfnrr',
+  address: '10 Clifton Gardens munich W9 1DT',
+  description: 'Alit shit, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 20,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'yeaskfnrr',
+  address: '10 Clifton Gardens munich W9 1DT',
+  description: 'Alit shit, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 20,
+  number_of_guests: 3
+)
